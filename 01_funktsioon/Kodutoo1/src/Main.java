847cc75ff68e08bd1054a52ee9297ab118a3b3c9
KVellik <55129189+KVellik@users.noreply.github.com>
2023-03-10 10:36:08 +0200

    kodutöö1

--- 01_funktsioon/Kodutoo1/src/Main.java
+++ 01_funktsioon/Kodutoo1/src/Main.java
@@ -1,3 +1,4 @@
+import java.text.DecimalFormat;
 import java.util.Scanner;
 
 public class Main {
@@ -20,13 +21,42 @@ public class Main {
         // Sõiduaeg
         Scanner scanner2 = new Scanner(System.in);
         System.out.println("Hetkene sõidukiirus (km/h): ");
-        int kiirus_hetkel = scanner.nextInt();
+        double kiirus_hetkel = scanner.nextInt();
         Scanner scanner3 = new Scanner(System.in);
         System.out.println("Distant sihtkohta (km): ");
-        int distants = scanner.nextInt();
-        float Teekond = (distants / kiirus_hetkel);
-        System.out.println("Sihtkohta jõudmiseks kulub " + Teekond + " tundi.");
+        double distants = scanner.nextInt();
+        double Teekond = (distants / kiirus_hetkel);
+        System.out.println("Sihtkohta jõudmiseks kulub " + df.format(Teekond) + " tundi.");
 
+        // Palgakalkulaator
+        double neto = arvutaNetopalk(2000);
+        System.out.println("Netopalk on " + neto);
+
+        // Tankimine
+        double summa = arvutaKütuseHind(40);
+        System.out.println("Kütuse maksumus oli " + summa + " eurot");
+
+        // Valuutakalkulaator
+        double raha = arvutaNaelteks(150);
+        System.out.println("150 eurot Suurbritannia naeltest on " + raha + " naela");
+    }
+
+    private static double arvutaNaelteks(double eurod) {
+        double kurss = 0.88;
+        return eurod * kurss;
+    }
+
+    private static double arvutaNetopalk(double bruto){
+        double tulumaksuvabamin = 654;
+        double töötuskindlustus = bruto * 0.016;
+        double kogumispension = bruto * 0.02;
+        double tulumaks = bruto - tulumaksuvabamin - töötuskindlustus - kogumispension;
+        return (bruto - töötuskindlustus - kogumispension - tulumaks);
+    }
+
+    private static double arvutaKütuseHind(double liitrid) {
+        double kütuseHind = 1.72;
+        return kütuseHind * liitrid;
     }
 
     private static String Kiiruskaamera(int kiirus) {
@@ -36,6 +66,8 @@ public class Main {
             return "Lubatud";
         }
     }
+
+    private static final DecimalFormat df = new DecimalFormat("0.0");
 }
 
 
