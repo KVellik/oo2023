64937646cd27f18c76ef0197ee8f73aeb1f0fa12
KVellik <55129189+KVellik@users.noreply.github.com>
2023-03-17 14:58:09 +0200

    tmp

--- 05_kontrolltoo1/KT1/src/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        String lause = "Tere, maailm!";
-        int nihe = 1;
-        String cipherLause = Cipher.nihuta(lause, nihe);
-        System.out.println("Algse lause \"" + lause + "\" nihutatud lauseks nihe " + nihe + " korral: ");
-        System.out.println(cipherLause);
-    }
-}
